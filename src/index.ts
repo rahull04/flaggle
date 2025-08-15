b67438c401dfb5fc3f51b9769d2fc97cde00223f
Rahul Bandodkar <rahulbandodkar97@gmail.com>
2025-08-15 15:36:59 +0530

    minor fix

--- src/index.ts
+++ src/index.ts
@@ -22,3 +22,4 @@ export * from "./adapters/postgresAdapter";
 export * from "./adapters/mysqlAdapter";
 export * from "./adapters/mongoAdapter";
 export * from "./adapters/memoryAdapter";
+export * from "./expressRouter";
